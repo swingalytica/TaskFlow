02ec4ef5fd17d8f5f173524488e3a88d543f9fe0
Julian Hammer <j.hammer@swingalytica.com>
2026-07-22 15:17:13 +0200

    feat: define card and label schemas with required fields and timestamps

--- src/lib/server/mongodb/models/card.ts
+++ src/lib/server/mongodb/models/card.ts
@@ -1,14 +1,29 @@
 import mongoose from 'mongoose';
 
-const card_schema = new mongoose.Schema(
-	{
-		title: { type: String, required: true },
-		board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
-		column: { type: mongoose.Schema.Types.ObjectId, required: true },
-		order: { type: Number, required: true }
+const card_schema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
 	},
-	{ timestamps: true }
-);
+
+	board: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Board',
+		required: true
+	},
+
+	column: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true
+	},
+
+	labels: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Label'
+		}
+	]
+});
 
 card_schema.index({
 	board: 1,
