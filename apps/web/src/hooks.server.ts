31b70130b8fd166c08f979ced4dea28381e5c42a
Julian Hammer <j.hammer@swingalytica.com>
2026-07-23 17:01:13 +0200

    feat: implement logout functionality and update header component with button elements

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -5,12 +5,13 @@ import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const is_protected = event.url.pathname.startsWith('/app');
+	const public_routes = ['/login', '/register', '/logout'];
 	const auth = authenticate(event.cookies);
 
 	if (is_protected && !auth) {
 		event.cookies.delete('email', cookie_options);
 		throw redirect(307, '/');
-	} else if (auth && !is_protected) {
+	} else if (auth && !is_protected && !public_routes.includes(event.url.pathname)) {
 		throw redirect(307, '/app');
 	}
 
